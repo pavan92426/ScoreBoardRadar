c653035ee35cc439496f0348fe3f3458c73f2d22
Pavan Kumar Theegala <pavankumar.theegala@gmail.com>
2024-04-22 07:28:04 +0200

    added code coverage and unit test cases report

--- FootBallScore/src/main/java/org/score/theme/services/MatchService.java
+++ FootBallScore/src/main/java/org/score/theme/services/MatchService.java
@@ -2,8 +2,6 @@ package org.score.theme.services;
 
 import org.score.theme.Match;
 import org.score.theme.models.ScoreBoard;
-
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -18,21 +16,27 @@ public class MatchService implements Match {
 
     /**
      * A match started will be added to the live matches active list.
-     * @return
+     *
+     * @return match
      */
     @Override
     public List<ScoreBoard> startMatch(ScoreBoard match) {
-        System.out.println("Entering start match:"+matches.size());
+        System.out.println("Entering start match:" + matches.size());
         if (match.isGameStatus()) {
             matches.add(match);
         }
-        System.out.println("Exiting start match:"+matches.size());
+        System.out.println("Exiting start match:" + matches.size());
         return matches;
     }
 
+    /**
+     * update match receives updates of ongoing match.
+     * @param matchID, homeScore, awayScore
+     * @return void
+     */
     @Override
     public void updateMatch(int matchID, int homeScore, int awayScore) {
-        System.out.println("Entering update match with size"+matches.size());
+        System.out.println("Entering update match with size" + matches.size());
         var getMatch = matches.stream().filter(scoreBoard -> scoreBoard.getGameID() == matchID).findFirst();
         getMatch.ifPresentOrElse(scoreBoard -> {
             scoreBoard.setHomeScore(homeScore);
@@ -40,11 +44,16 @@ public class MatchService implements Match {
         }, () -> {
             throw new NoSuchElementException("Not able to find a active match to update the score");
         });
-        System.out.println("Exiting update match with size"+matches.size());
+        System.out.println("Exiting update match with size" + matches.size());
     }
 
+    /**
+     * finish match will remove the match from score board.
+     * @param matchID, status
+     * @return void
+     */
     @Override
-    public void finishMatch(int matchID,boolean status) {
+    public void finishMatch(int matchID, boolean status) {
         var getMatch = matches.stream().filter(scoreBoard -> scoreBoard.getGameID() == matchID).findFirst();
         getMatch.ifPresentOrElse(scoreBoard -> {
             scoreBoard.setGameStatus(status);
@@ -54,18 +63,22 @@ public class MatchService implements Match {
         matches.remove(getMatch.get());
     }
 
-
+    /**
+     * board summary keep changes based on same total scores.
+     * @param matches
+     * @return matches
+     */
     @Override
     public List<ScoreBoard> boardSummary(List<ScoreBoard> matches) {
         System.out.println("Entering board summary");
-       matches.sort(Comparator.comparingInt(ScoreBoard::getTotalScore)
-               .reversed()
-                .thenComparingInt(ScoreBoard::getGameID)
-               //.reversed()
+        matches.sort(Comparator.comparingInt(ScoreBoard::getTotalScore)
+                        .reversed()
+                        .thenComparingInt(ScoreBoard::getGameID)
+                //.reversed()
         );
 
-        matches.forEach(val -> System.out.println(val.getHomeTeam()+""+val.getHomeScore()
-                +"-"+val.getAwayTeam()+""+val.getAwayScore()+" - "+val.getGameID()));
+        matches.forEach(val -> System.out.println(val.getHomeTeam() + "" + val.getHomeScore()
+                + "-" + val.getAwayTeam() + "" + val.getAwayScore() + " - " + val.getGameID()));
         System.out.println("Exiting board summary");
         return matches;
     }
